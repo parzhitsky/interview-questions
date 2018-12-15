390b8b0e60b6d0e8d1d732b8f0f1c6a06d05be73
Dmitry Parzhitsky <parzhitsky@gmail.com>
2018-12-15 15:25:35 +0200

    add bubble-sort algorithm

---
+++ src/algorithms/bubble-sort.ts
@@ -0,0 +1,11 @@
+/** sort a given list of numbers using BubbleSort technique */
+export default function bubbleSort(list: number[]): number[] {
+	const { length } = list;
+
+	for (let end = 0; length - end > 1; end ++)
+		for (let right = length - 1; right > end; right--)
+			if (list[right - 1] > list[right])
+				[ list[right - 1], list[right] ] = [ list[right], list[right - 1] ];
+
+	return list;
+};
