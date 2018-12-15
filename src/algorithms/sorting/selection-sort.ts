385bed54dbf040ca6abd699977ef23d0bd259da2
Dmitry Parzhitsky <parzhitsky@gmail.com>
2018-12-15 17:35:35 +0200

    optimize selection-sort

--- src/algorithms/sorting/selection-sort.ts
+++ src/algorithms/sorting/selection-sort.ts
@@ -1,15 +1,14 @@
 /** sorts a given list of numbers using SelectionSort strategy */
 export default function selectionSort(list: number[]): number[] {
-	if (list.length > 1)
-		for (let left = 0; left < list.length; left++) {
-			let min = left;
+	for (let left = 0; left < list.length; left++) {
+		let min = left;
 
-			for (let next = left + 1; next < list.length; next++)
-				if (list[next] < list[min])
-					min = next;
+		for (let next = left + 1; next < list.length; next++)
+			if (list[next] < list[min])
+				min = next;
 
-			[ list[left], list[min] ] = [ list[min], list[left] ];
-		}
+		[ list[left], list[min] ] = [ list[min], list[left] ];
+	}
 
 	return list;
 }
