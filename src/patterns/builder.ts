0185c7c0dfb50e27e3c1757a2aa14c6a434a8cf8
Dima Parzhitsky <parzhitsky@gmail.com>
2023-01-23 14:19:39 +0200

    Describe Builder pattern

---
+++ src/patterns/builder.ts
@@ -0,0 +1,77 @@
+export {}
+
+abstract class Named {
+  readonly name = this.constructor.name
+}
+
+abstract class HousePart extends Named {}
+
+class Fence extends HousePart {}
+class SwimmingPool extends HousePart {}
+class Porch extends HousePart {}
+
+class House extends Named {
+  private parts: HousePart[] = []
+
+  addPart(part: HousePart): void {
+    this.parts.push(part)
+  }
+}
+
+class HouseBuilder {
+  private built = false
+
+  constructor(private house = new House()) {}
+
+  addPart(part: HousePart): this {
+    this.house.addPart(part)
+
+    return this
+  }
+
+  build(): House {
+    if (this.built) {
+      throw new Error('Cannot reuse builder')
+    }
+
+    this.built = true
+
+    return this.house
+  }
+}
+
+export const house1: House = new House()
+export const house2: House = new HouseBuilder().build()
+
+export const houseWithFence: House = new HouseBuilder()
+  .addPart(new Fence())
+  .build()
+
+export const houseWithPool: House = new HouseBuilder()
+  .addPart(new SwimmingPool())
+  .build()
+
+export const houseWithPorch: House = new HouseBuilder()
+  .addPart(new Porch())
+  .build()
+
+export const houseWithFenceAndPool: House = new HouseBuilder()
+  .addPart(new Fence())
+  .addPart(new SwimmingPool())
+  .build()
+
+export const houseWithPoolAndPorch: House = new HouseBuilder()
+  .addPart(new SwimmingPool())
+  .addPart(new Porch())
+  .build()
+
+export const houseWithPorchAndFence: House = new HouseBuilder()
+  .addPart(new Porch())
+  .addPart(new Fence())
+  .build()
+
+export const houseWithFenceAndPoolAndPorch: House = new HouseBuilder()
+  .addPart(new Fence())
+  .addPart(new SwimmingPool())
+  .addPart(new Porch())
+  .build()
